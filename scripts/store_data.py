cf47d03edfd664ed62536db13846af906cd6864f
serkalemnegusse1 <serkalemnegusse1@gmail.com>
2025-02-01 14:40:12 +0300

    update sqlalchemy

--- scripts/store_data.py
+++ scripts/store_data.py
@@ -13,7 +13,7 @@ def conn():
     db_user = os.getenv('DB_USER')
     db_password = os.getenv('DB_PASSWORD')
     db_host = os.getenv('DB_HOST')
-    db_port = os.getenv('DB_PORT')
+    db_port = os.getenv('DB_PORT') 
     db_name = os.getenv('DB_NAME')
 
     # Create SQLAlchemy engine
